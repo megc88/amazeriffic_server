c0c976ced9b0f00affbcc51c2805354ba8eb62e9
Megan Conlin <Megan Conlin>
2016-12-04 12:50:09 -0800

    to do list via the server

--- client/javascripts/app.js
+++ client/javascripts/app.js
@@ -10,10 +10,10 @@ var main = function(toDoObjects) {
     
 
     $(element).on("click", function() {
-      var $content,
-      var $input,
-      var $button,
-      var i;
+       var $content,
+           $input,
+           $button,
+           i;
 
     $(".tabs a span").removeClass("active");
     $element.addClass("active");
@@ -21,25 +21,26 @@ var main = function(toDoObjects) {
 
 
     if ($element.parent().is(":nth-child(1)")) {
-      $content = $("<ul>");
-      for(i = toDos.length-1; i >= 0; i--) {
-        $content.append($("<li>").text(toDos[i]));
+       $content = $("<ul>");
+       for(i = toDos.length-1; i >= 0; i--) {
+          $content.append($("<li>").text(toDos[i]));
        }
- } else if ($element.parent().is(":nth-child(2)")) {
-     $content = $("<ul>");
-     toDos.forEach(function (todo) {
-       $content.append($("<li>").text(todo));
-     });
- } else if ($element.parent().is(":nth-child(3)")) {
-     var tags = [];
-
-     toDoObjects.forEach(function (toDo) {
-       toDo.tags.forEach(function (tag) {
-          if (tags.indexOf(tag) === -1) {
-	     tags.push(tag);
-	  }
+   } else if ($element.parent().is(":nth-child(2)")) {
+       $content = $("<ul>");
+       toDos.forEach(function (todo) {
+          $content.append($("<li>").text(todo));
        });
-     });
+   
+   } else if ($element.parent().is(":nth-child(3)")) {
+       var tags = [];
+
+       toDoObjects.forEach(function (toDo) {
+         toDo.tags.forEach(function (tag) {
+            if (tags.indexOf(tag) === -1) {
+	       tags.push(tag);
+	    }
+         });
+       });
      console.log(tags);
 
      var tagObjects = tags.map(function (tag) {
@@ -50,10 +51,12 @@ var main = function(toDoObjects) {
 	    toDosWithTag.push(toDo.description);
 	  }
 	});
+         return { "name" : tag, "toDos" : toDosWithTag };
+    });	
 
      tagObjects.forEach(function (tag) {
        var $tagName = $("<h3>").text(tag.name),
-       var $content = $("<ul>");
+           $content = $("<ul>");
 
        tag.toDos.forEach(function (description) {
          var $li = $("<li>").text(description);
@@ -62,19 +65,40 @@ var main = function(toDoObjects) {
 
        $("main .content").append($tagName);
        $("main .content").append($content);
-       });
+    });
+   
  } else if ($element.parent().is(":nth-child(4)")) {
-     $input = $("<input>"),
-     $button = $("<button>").text("+");
+     var $input = $("<input>").addClass("description"),
+         $inputLabel = $("<p>").text("Description: "),
+     	 $tagInput = $("<input>").addClass("tags"),
+     	 $tagLabel = $("<p>").text("Tags: "),
+     	 $button = $("<button>").text("+");
 
-     $ibutton.on("click", function () {
-       if ($input.val() !== "") {
-          toDos.push($input.val());
-          $input.val("");
-        }
-   });
-   $content = $("<div>").append($input, $button);
+     $button.on("click", function () {
+       var description = $input.val(),
+          tags = $tagInput.val().split(","),
+	  newToDo = {"description":description, "tags":tags};
 
+	toDoObjects.push({"description":description, "tags":tags});
+
+	$.post("todos", newToDo, function(response) {
+	   console.log("We posted and the server responded!");
+	   console.log(response);
+	});
+
+	toDos = toDoObjects.map(function(toDo) {
+	   return toDo.description;
+	});
+	
+	$input.val("");
+	$tagInput.val("");
+      });	
+
+   	$content = $("<div>").append($inputLabel)
+			     .append($input)
+			     .append($tagLabel)
+			     .append($tagInput)
+			     .append($button);
    }
 
    $("main .content").append($content);
@@ -82,15 +106,13 @@ var main = function(toDoObjects) {
      return false;
 
    });
-
- });
+  });
    
    $(".tabs a:first-child span").trigger("click");
-
 };
 
 $(document).ready(function () {
-   $.getJSON("todo.json", function (toDoObjects) {
+   $.getJSON("todos.json", function (toDoObjects) {
       main(toDoObjects);
    });
 });
