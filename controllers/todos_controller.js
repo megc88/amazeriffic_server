c18ad3b1e508398a3f52296e9ccff650693b12b8
Megan Conlin <Megan Conlin>
2016-12-19 22:16:35 -0800

    set up route for todos IDs

--- controllers/todos_controller.js
+++ controllers/todos_controller.js
@@ -7,6 +7,24 @@ ToDosController.index = function (req, res) {
    });
 };
 
+ToDosController.show = function(req, res) {
+   var id = req.params.id;
+
+   ToDo.find({"_id":id}, function (err, todo) {
+      if (err!== null) {
+         res.json(err);
+      } else {
+         if (todo.length > 0) {
+            res.json(todo[0]);
+         } else {
+            res.send("Not Found");
+         }
+      }
+   });
+};
+
+    
+
 ToDosController.create = function (req, res) {
    var newToDo = new ToDo({"description":req.body.description, 
                            "tags":req.body.tags});
--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use(express.static(__dirname + "/client"));
 app.use(express.urlencoded());
 
 if (process.env.VCAP_SERVICES) {
-   mongoUrl = "mongodb://CloudFoundry_jkums0ap_gmp0331h_l6q1rnju:7Uqo06lqf6jzjUjCpCHAbXJNbof1IOzr@ds051833.mlab.com:51833/CloudFoundry_jkums0ap_gmp0331h" 
+   mongoUrl = "mongodb://CloudFoundry_jkums0ap_gmp0331h_l6q1rnju:7Uqo06lqf6jzjUjCpCHAbXJNbof1IOzr@ds051833.mlab.com:51833/CloudFoundry_jkums0ap_gmp0331h"; 
 
 } else {
    mongoUrl = "mongodb://localhost/amazeriffic";
@@ -21,6 +21,8 @@ mongoose.connect(mongoUrl);
 
 
 http.createServer(app).listen(port);
+
+app.get("/todos/:id", ToDosController.show);
 
 app.get("/todos.json", ToDosController.index);
 
